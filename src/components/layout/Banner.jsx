6785fbdf5e91b73c0a327f5ba13d0c7f4f62554b
helen tan <helentanmz0909@gmail.com>
2022-07-15 18:45:24 +0800

    Added banner text responsiveness for sm & md screens

--- src/components/layout/Banner.jsx
+++ src/components/layout/Banner.jsx
@@ -1,9 +1,9 @@
 function Banner() {
   return (
-    <div className="banner content-center">
+    <div className="banner justify-center items-center">
       <div className="text-grey">
-        <h1 className="">Hi, I'm Helen</h1>
-        <p>Web Developer</p>
+        <h1 className="mb-5 text-4xl md:text-7xl">Hi, I'm Helen</h1>
+        <p className="text-center text-xl md:text-3xl">Web Developer</p>
       </div>
     </div>
   )
