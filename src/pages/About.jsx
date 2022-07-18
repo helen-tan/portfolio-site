fe697d128816954079e2f80fb4ae211ef9e9a656
helen tan <helentanmz0909@gmail.com>
2022-07-18 18:30:45 +0800

    Updated about content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,23 +11,28 @@ function About() {
         </div>
         <div className="about-text-wrapper w-100 md:w-7/12">
           <p className="text-lg mb-4">
-            My interest in web development started back in 2017 as a second year student at the National University
+            My interest in web development started back in 2017 as a second year Engineering student at the National University
             of Singapore, when I attended a Bootstrap workshop and learned how to remove an annoying popups by deleting
             it from the inspect elements page. Ever since, I have been learning on my own how to develop websites.
           </p>
           <p className="text-lg mb-4">
-            After having the privilege of working as a technology business analyst for a year after graduation,
-            I cemented my decision to become a web developer, attended a coding bootcamp. At the end of the day,
+            After having the privilege of working as a <span className='text-teal'>technology business analyst</span> for a year after graduation,
+            I experienced the process of gathering requirements and translating them into a wireframe. It quickly became apparent that
+            understanding how code works is key to becoming an effective member on a tech team, so as to communicate with other devs in
+            their lingo, or to judge whether certain features are feasible.
+          </p>
+          <p className="text-lg mb-4">
+            I cemented my decision to become a web developer, attended a <span className='text-teal'>coding bootcamp</span> to learn the fundamentals, and everyday I strive to learn a little more about the craft. At the end of the day,
             I aim to understand how things work under the hood so as to write code that not only functions,
             but also understood by other developers.
           </p>
           <p className="text-lg mb-4">
             I consider work an ongoing education, and I hope to be with people who are willing to share their knowledge
-            as much as I want to learn. I aspire towards a career where I can create beautiful products using good software
-            practices, with people that bring out the best in each other.
+            as much as I want to learn. I aspire towards a career where I can create useful products using good software
+            practices, with people who bring out the best in each other.
           </p>
           <p className="text-lg mb-4">
-          <strong className='text-teal'>When I’m not in front of a computer screen</strong>, I could be reading, petting cats, gaming on my PS4, or wandering some
+          <strong className='text-teal'>When I’m not in front of a computer screen</strong>, I could be reading, petting cats, gaming on my PS4, queuing for bubble tea, or wandering some
           part of Singapore that I haven’t been to.
           </p>
         </div>
