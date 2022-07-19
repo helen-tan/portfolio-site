3a09600e5f3458ef10f545a2c666ac37f37b3fa8
helen tan <helentanmz0909@gmail.com>
2022-07-19 17:03:35 +0800

    setup timeline, timeline card, timeline icon

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,18 +1,36 @@
+import { GiGraduateCap } from "react-icons/gi";
+
 function Experience() {
   return (
     <div id='experience' className='text-grey'>
       <div className='bg'></div>
 
       <div className="experience-wrapper px-6 py-24 md:p-24">
-        <div className='flex flex-col justify-center items-center text-center'>
+        <div className='flex flex-col justify-center items-center text-center mb-10'>
           <h2 className='text-5xl font-semibold'>Experience</h2>
           <span className='divider h-0.5 bg-teal w-40 mr-auto ml-auto my-8'></span>
-          <p className="text-lg mb-4">
+          <p className="text-lg">
             I am a graduate from the class of 2020 at the National University of Singapore,
             majoring in Material Science and Engineering. At NUS, the rigor of the engineering
             academic program has equipped me with an analytical and logical thinking towards
             tackling problems.
           </p>
+        </div>
+
+        <div className="timeline-wrapper">
+          <div className="timeline">
+
+            <div className="timeline-block">
+              <div className="timeline-icon">
+                <GiGraduateCap size={28}/>
+              </div>
+            </div>
+
+            <div className="timeline-card">
+              <h2>Full Stack Web Developer Student</h2>
+              <h3>Le Wagon, Singapore</h3>
+            </div>
+          </div>
 
         </div>
       </div>
