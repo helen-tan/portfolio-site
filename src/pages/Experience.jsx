0e59405044276a97893bb47cbefcd53052af895b
helen tan <helentanmz0909@gmail.com>
2022-07-19 15:35:20 +0800

    Added experience header and description

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,7 +1,23 @@
 function Experience() {
   return (
-    <div id='experience' className='h-screen'>
-      Experience
+    <div id='experience' className='text-grey'>
+      <div className='bg'></div>
+
+      <div className="experience-wrapper px-6 py-24 md:p-24">
+        <div className='flex flex-col justify-center items-center text-center'>
+          <h2 className='text-5xl font-semibold'>Experience</h2>
+          <span className='divider h-0.5 bg-teal w-40 mr-auto ml-auto my-8'></span>
+          <p className="text-lg mb-4">
+            I am a graduate from the class of 2020 at the National University of Singapore,
+            majoring in Material Science and Engineering. At NUS, the rigor of the engineering
+            academic program has equipped me with an analytical and logical thinking towards
+            tackling problems.
+          </p>
+
+        </div>
+      </div>
+
+
     </div>
   )
 }
