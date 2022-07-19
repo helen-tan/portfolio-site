6e80547e3b8091b9888d745589b02a2eb5fefa0c
helen tan <helentanmz0909@gmail.com>
2022-07-19 11:01:10 +0800

    Added dark grey color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
       colors: {
         'black': '#1B1B1B',
         'teal': '#00B7C7',
-        'grey': '#F2F2F2'
+        'grey': '#F2F2F2',
+        'dark-grey': '#333333'
       }
     },
   },
