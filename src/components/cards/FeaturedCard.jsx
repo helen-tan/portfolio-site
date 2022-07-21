e2778720ae07862babcf2a3d0c1772e65d20e9c2
helen tan <helentanmz0909@gmail.com>
2022-07-21 14:27:20 +0800

    featured card content

--- src/components/cards/FeaturedCard.jsx
+++ src/components/cards/FeaturedCard.jsx
@@ -1,6 +1,36 @@
 function FeaturedCard() {
   return (
-    <div>FeaturedCard</div>
+    <div className="featured-card">
+      <div className="screenshots">
+
+      </div>
+      <div className="featured-header flex justify-between py-3 bg-slate-700">
+        <h3 className="text-2xl ml-3">CHUM</h3>
+        <div className="flex">
+          <a href="https://www.chum.voyage/" target="_blank" rel="noreferrer" className='btn btn-transparent-teal btn-sm mx-2'>Demo</a>
+          <a href="https://github.com/rayjosong/rails-chum" target="_blank" rel="noreferrer" className='btn btn-transparent-teal btn-sm mx-2'>Code</a>
+        </div>
+      </div>
+
+      <div className="featured-description w-full py-3">
+        <p>
+        During the bootcamp at Le Wagon, I’ve designed and developed CHUM, a Ruby on Rails web application with 3 other teammates. User can plan trips and post itineraries for other people to join, or join a trip posted by others. Having faced the difficulty of planning overseas trips with friends due to conflicting schedules, we spun up this project during our project week.
+        </p>
+      </div>
+
+      <div className="used">
+        <ul className="used-items">
+          <li className="used-item">HTML</li>
+          <li className="used-item">SCSS</li>
+          <li className="used-item">Bootstrap</li>
+          <li className="used-item">JavaScript</li>
+          <li className="used-item">StimulusJS</li>
+          <li className="used-item">Ruby on Rails</li>
+          <li className="used-item">Cloudinary</li>
+          <li className="used-item">PostgreSQL</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,16 @@
+import FeaturedCard from "../components/cards/FeaturedCard"
+
 function Portfolio() {
   return (
     <div id='portfolio' className='bg-black text-grey h-auto px-6 py-24 md:p-24'>
       <h2 className='text-3xl font-semibold text-center md:text-4xl md:text-left'>Some Things I've Built</h2>
       <span className='divider h-0.5 bg-teal w-40 mx-auto md:ml-0'></span>
+
+      <div className="portfolio-wrapper">
+        <FeaturedCard />
+
+      </div>
+
     </div>
   )
 }
