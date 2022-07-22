e05425224a1e7fa239032212c7be9ec7eb9e1298
helen tan <helentanmz0909@gmail.com>
2022-07-22 23:10:34 +0800

    align tags to bottom of card

--- src/components/cards/ProjectCard.jsx
+++ src/components/cards/ProjectCard.jsx
@@ -2,9 +2,9 @@ import { FiFolder, FiExternalLink, FiGithub } from "react-icons/fi";
 
 function ProjectCard({ project }) {
   return(
-    <div className="project-card bg-dark-grey p-8 rounded group transition duration-300 ease-in-out hover:-translate-y-2">
+    <div className="project-card flex flex-col flex-none bg-dark-grey p-8 rounded group transition duration-300 ease-in-out hover:-translate-y-2">
 
-      <div className="project-card-top flex justify-between items-center mb-3">
+      <div className="project-card-top flex justify-between items-center mb-3 flex-none">
         <FiFolder className='text-teal text-4xl' />
         <div className="project-links flex items-center">
           {(project.code_link !== "") ?
@@ -16,22 +16,27 @@ function ProjectCard({ project }) {
         </div>
       </div>
 
-      <h3 className="mb-3 font-bold text-2xl group-hover:text-teal">
+      <h3 className="mb-3 font-bold text-2xl flex-none group-hover:text-teal">
         <a href={project.demo_link} target="_blank" rel="noreferrer">
           {project.title}
         </a>
       </h3>
 
-      <div className="project-description w-full">
+      <div className="project-description w-full flex-none">
         <p>{project.desc}</p>
       </div>
 
-      <div className="used">
-        <ul className="used-items">
-          {project.used_items.map((item) => {
-            return <li className="used-item" key={item}>{item}</li>
-          })}
-        </ul>
+      <div className="used grow">
+        <div className="flex h-full items-end">
+          <ul className="used-items ">
+            {project.used_items.map((item) => {
+              return <li className="used-item" key={item}>{item}</li>
+            })}
+          </ul>
+
+        </div>
+
+
       </div>
     </div>
   )
